1b911e5be3b020b006d98aa3aa0180260099b35f
edutilos666 <edutilosaghayev@gmail.com>
2017-10-02 23:53:48 +0200

    1

---
+++ myNotes/module example/Arithmetic.fs
@@ -0,0 +1,46 @@
+﻿module main.Arithmetic 
+let add x y =
+   x + y
+
+let sub x y =
+   x - y
+       
+let mult x y =
+   x * y
+
+let div x y =
+   x / y
+
+
+type Worker(id:int64, name:string , age:int32, wage:double, active:bool) = 
+    let mutable _id = id 
+    let mutable _name = name 
+    let mutable _age = age 
+    let mutable _wage = wage
+    let mutable _active = active 
+
+    member self.Id
+       with get() = _id 
+       and set(value) = _id <- value 
+
+    member self.Name 
+       with get() = _name 
+       and set(value) = _name <- value 
+
+    member self.Age 
+       with get() = _age 
+       and set(value) = _age <- value 
+
+    member self.Wage 
+       with get() = _wage 
+       and set(value) = _wage <- value 
+
+    member self.Active 
+       with get() = _active 
+       and set(value) = _active <- value 
+
+    override self.ToString() = 
+        sprintf "Worker(%d,%s,%d,%.2f,%b)" self.Id self.Name self.Age self.Wage self.Active
+
+
+
---
+++ myNotes/module example/Program.fs
@@ -0,0 +1,58 @@
+﻿module main.Program 
+
+
+let testModule1 = 
+    let x1:int = 10 
+    let x2:int = 3 
+    let add = Arithmetic.add x1 x2 
+    let sub = Arithmetic.sub x1 x2
+    let mult = Arithmetic.mult x1 x2 
+    let div = Arithmetic.div x1 x2 
+    printfn "<<Arithmetic Resulst>>" 
+    printfn "x1 = %d , x2 = %d " x1 x2 
+    printfn "x1 + x2 = %d" add
+    printfn "x1 - x2 = %d" sub 
+    printfn "x1 * x2 = %d" mult 
+    printfn "x1 / x2 = %d" div 
+
+
+open Arithmetic
+let testModule2 = 
+
+    let x1 = 10 
+    let x2 = 3 
+    let res_add = add x1 x2 
+    let res_sub = sub x1 x2 
+    let res_mult = mult x1 x2 
+    let res_div = div x1 x2 
+    printfn "<<Arithmetic Operators 2>>"
+    printfn "x1 = %d , x2 = %d" x1 x2 
+    printfn "x1 + x2 = %d" res_add 
+    printfn "x1 - x2 = %d" res_sub 
+    printfn "x1 * x2 = %d" res_mult 
+    printfn "x1 / x2 = %d" res_div
+    let w1 = new Worker(1L , "foo", 10, 100.0, true) 
+    printfn "w1 = %s" (w1.ToString())
+    w1.Id <- 2L 
+    w1.Name <- "new foo"
+    w1.Age <- 20 
+    printfn "w1 = %s" (w1.ToString()) 
+
+
+
+module module1 = 
+   let name = "foo" 
+
+module module2 = 
+   let name = "bar"
+
+
+[<EntryPoint>]
+let main argv = 
+    testModule1
+    printfn ""
+    testModule2
+    printfn "module1.name = %s" (module1.name)
+    printfn "module2.name = %s" module2.name 
+    0
+   
